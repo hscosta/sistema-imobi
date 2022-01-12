8e0a4c83f9af0b2ac4d98332c41d6ec9961f5b5d
Heverton S Costa <hevertonscosta at gmail dot com>
2022-01-12 17:30:22 -0300

    Upd apps.plataforma - upd apps.plataforma.admin add class ImovelAdmin with list params

--- apps/plataforma/admin.py
+++ apps/plataforma/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 from apps.plataforma.models import *
 # Imagem, Cidade, DiasVisita, Horario, Imovel, Visitas
 
+
+@admin.register(Imovel)
+class ImovelAdmin(admin.ModelAdmin):
+    list_display = (
+        'rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo', 'tipo_imovel',
+    )
+    list_editable = (
+        'valor', 'tipo',
+    )
+    list_filter = (
+        'cidade', 'tipo', 'tipo_imovel',
+    )
+
+
 admin.site.register(Imagem)
 admin.site.register(Cidade)
 admin.site.register(DiasVisita)
 admin.site.register(Horario)
-admin.site.register(Imovel)
+# admin.site.register(Imovel)
 admin.site.register(Visitas)
