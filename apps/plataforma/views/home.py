64b0bd11cc84cfebd630b6f9fdea141e05943a3e
Heverton S Costa <hevertonscosta at gmail dot com>
2022-01-12 17:50:51 -0300

    Upd apps.plataforma and .templates - upd home.html add filtrar modal - upd home view adding cidades on context

--- apps/plataforma/views/home.py
+++ apps/plataforma/views/home.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from apps.plataforma.models import Imovel
+from apps.plataforma.models import Imovel, Cidade
 
 
 # home.index
 @login_required(login_url='/auth/logar')
 def index(request):
     imoveis = Imovel.objects.all()
+    cidades = Cidade.objects.all()
+
     context = {
         'imoveis': imoveis,
+        'cidades': cidades,
     }
 
     return render(request, 'plataforma/home.html', context)
