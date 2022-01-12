0f84be7e33d5b209b75d6e8ef2fbb219d65ed4d5
Heverton S Costa <hevertonscosta at gmail dot com>
2022-01-12 18:35:49 -0300

    Upd apps.plataforma - add filter on home.index view to select imoveis - add button 'LIMPAR FILTRO' on home.html

--- apps/plataforma/views/home.py
+++ apps/plataforma/views/home.py
@@ -7,8 +7,31 @@ from apps.plataforma.models import Imovel, Cidade
 # home.index
 @login_required(login_url='/auth/logar')
 def index(request):
-    imoveis = Imovel.objects.all()
+    preco_minimo = request.GET.get('preco_minimo')
+    preco_maximo = request.GET.get('preco_maximo')
+    cidade = request.GET.get('cidade')
+    tipo = request.GET.getlist('tipo')
     cidades = Cidade.objects.all()
+
+    # if not preco_minimo or not preco_maximo or not tipo:
+    #     imoveis = Imovel.objects.all()
+
+    if preco_minimo or preco_maximo or tipo:
+
+        if not preco_minimo:
+            preco_minimo = 0
+        if not preco_maximo:
+            preco_maximo = 999999999
+        if not tipo:
+            tipo = ['A', 'C']
+
+        imoveis = Imovel.objects.\
+            filter(valor__gte=preco_minimo).\
+            filter(valor__lte=preco_maximo).\
+            filter(tipo_imovel__in=tipo).\
+            filter(cidade=cidade)
+    else:
+        imoveis = Imovel.objects.all()
 
     context = {
         'imoveis': imoveis,
