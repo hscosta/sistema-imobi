dc64c9e8df57d1d59227e0933eea0296bec40470
Heverton S Costa <hevertonscosta at gmail dot com>
2022-01-12 17:36:40 -0300

    Upd apps.plataforma - upd views.home to render plataforma.home.html

--- apps/plataforma/views/home.py
+++ apps/plataforma/views/home.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from apps.plataforma.models import Imovel
 
 
 # home.index
 @login_required(login_url='/auth/logar')
 def index(request):
-    return HttpResponse('Estamos na página Plataforma/Home')
+    imoveis = Imovel.objects.all()
+    context = {
+        'imoveis': imoveis,
+    }
+
+    return render(request, 'plataforma/home.html', context)
