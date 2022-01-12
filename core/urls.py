03aa67e4da2fc6643790dc922279a86f814ad679
Heverton S Costa <hevertonscosta at gmail dot com>
2022-01-12 10:59:54 -0300

    Upd core.urls - add path to plataforma app

--- core/urls.py
+++ core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('apps.autenticacao.urls')),
+    path('', include('apps.plataforma.urls')),
 ]
